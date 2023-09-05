854ba921afaf7179cbb8c7d67475b2a3797819c2
iamDynamic <imanyouness909@gmail.com>
2023-09-05 08:43:35 +0300

    initial

---
+++ main.js
@@ -0,0 +1,17 @@
+var GuessTheNb = document.getElementById("guess-the-nb")
+var fakestrongpasswordgeneration = document.getElementById("fake-strong-password-generation")
+var todolist = document.getElementById("to-do-list")
+var guesstheplayer = document.getElementById("guess-the-player")
+
+GuessTheNb.onclick = ()=>{
+
+}
+fakestrongpasswordgeneration.onclick = ()=>{
+
+}
+todolist.onclick = ()=>{
+          location.href = ''
+}
+guesstheplayer.onclick = ()=>{
+          location.href='https://footballplayerguessgame.netlify.app/'
+}
