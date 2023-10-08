cf46f961f6297f6367a63180ab48c3c3d04b6e36
iamDynamic <imanyouness909@gmail.com>
2023-10-08 16:24:38 +0300

    main

--- main.js
+++ main.js
@@ -2,16 +2,19 @@ var GuessTheNb = document.getElementById("guess-the-nb")
 var fakestrongpasswordgeneration = document.getElementById("fake-strong-password-generation")
 var todolist = document.getElementById("to-do-list")
 var guesstheplayer = document.getElementById("guess-the-player")
+var apps = document.getElementById("apps")
 
-GuessTheNb.onclick = ()=>{
+apps.addEventListener("click",()=>{
+          location.href = "index.html?2222"
+})
+urlString ="http://127.0.0.1:5501/index.html?id=9021&ali=saq"
+let paramString = urlString.split('?')[1];
+let queryString = new URLSearchParams(paramString);
 
+for (let pair of queryString.entries()) {
+   console.log("Key is: " + pair[0]);
+   console.log("Value is: " + pair[1]);
+   document.write("ali")
 }
-fakestrongpasswordgeneration.onclick = ()=>{
 
-}
-todolist.onclick = ()=>{
-          location.href = ''
-}
-guesstheplayer.onclick = ()=>{
-          location.href='https://footballplayerguessgame.netlify.app/'
-}
+
